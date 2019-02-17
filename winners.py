ddfe5cb6c18c5b5b233e4f9b933eff3a7547b857
dmcgrory20 <danielmcgrory2020@u.northwestern.edu>
2019-02-17 17:00:07 -0600

    winners formatted

--- winners.py
+++ winners.py
@@ -7,6 +7,9 @@ from nltk.tokenize import TweetTokenizer
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
+with open('gg2013.json') as json_file:
+    data = json.load(json_file)
+
 
 OFFICIAL_AWARDS_1315 = [
 'cecil b. demille award',
@@ -55,93 +58,94 @@ big_dict = [
 ["best television series - musical or comedy"]
 ]
 
+def winners(data, list_of_awards):
 
-
-def winners(data, award_data):
-    host_word = award_data
     stop_dict = ["golden", "globes", "goldenglobes", "awards", "tweet",
                 "tweets", "award", "rt", "@", "best", "motion", "actress",
                 "actor", "drama", "picture","mtvnews","television","series",
                 "foreign", "film","comedy","hbo","girlshbo", "|","|"]
 
-    final_dict = {}
-    final_dict2 = {}
     result = {}
 
-    for tweet_json in data:
-        tweet = tweet_json["text"]
-        tweet_lower = tweet_json["text"].lower()
-        if host_word in tweet_lower:
-            # print(host_word)
+    for award_name in list_of_awards:
+        host_word = award_name
 
-            sen1 = host_word.split()[-1]
-            # print(sen1)
-            token_split = tweet.split()
-            token_tweet = nltk.word_tokenize(tweet)
-            tagged_tweet = nltk.pos_tag(token_tweet)
-            # print(tagged_tweet)
+        final_dict = {}
+        final_dict2 = {}
 
-            token_tweet_l = nltk.word_tokenize(tweet_lower)
-            # print("S")
-            # print(token_tweet_l)
+        for tweet_json in data:
+            tweet = tweet_json["text"]
+            tweet_lower = tweet_json["text"].lower()
+            if host_word in tweet_lower:
+                # print(host_word)
 
-            if sen1 in token_tweet_l:
-                indexof = token_tweet_l.index(sen1)
+                sen1 = host_word.split()[-1]
+                # print(sen1)
+                token_split = tweet.split()
+                token_tweet = nltk.word_tokenize(tweet)
+                tagged_tweet = nltk.pos_tag(token_tweet)
+                # print(tagged_tweet)
 
-                temp_w = ""
-                count = 1
+                token_tweet_l = nltk.word_tokenize(tweet_lower)
+                # print("S")
+                # print(token_tweet_l)
 
-                if indexof+count < len(tagged_tweet) and (tagged_tweet[indexof + count][0] == ":" or tagged_tweet[indexof + count][0] == "-" or tagged_tweet[indexof + count][0].lower() == "is"):
-                    count += 1
+                if sen1 in token_tweet_l:
+                    indexof = token_tweet_l.index(sen1)
 
-                    while indexof + count < len(tagged_tweet):
-                        if ((tagged_tweet[indexof + count][0] not in stop_dict and (tagged_tweet[indexof + count][1] == "NNP" or tagged_tweet[indexof + count][1] == "NN") and tagged_tweet[indexof + count][0] != "–")) or tagged_tweet[indexof + count][1] == "CD":
-                            if temp_w == "":
-                                temp_w = tagged_tweet[indexof + count][0]
+                    temp_w = ""
+                    count = 1
+
+                    if indexof+count < len(tagged_tweet) and (tagged_tweet[indexof + count][0] == ":" or tagged_tweet[indexof + count][0] == "-" or tagged_tweet[indexof + count][0].lower() == "is"):
+                        count += 1
+
+                        while indexof + count < len(tagged_tweet):
+                            if ((tagged_tweet[indexof + count][0] not in stop_dict and (tagged_tweet[indexof + count][1] == "NNP" or tagged_tweet[indexof + count][1] == "NN") and tagged_tweet[indexof + count][0] != "–")) or tagged_tweet[indexof + count][1] == "CD":
+                                if temp_w == "":
+                                    temp_w = tagged_tweet[indexof + count][0]
+                                else:
+                                    temp_w = temp_w + " " + tagged_tweet[indexof + count][0]
+                                count += 1
                             else:
-                                temp_w = temp_w + " " + tagged_tweet[indexof + count][0]
-                            count += 1
+                                break
+
+                    # print(temp_w)
+                    temp_w = temp_w.lower()
+
+                    if temp_w != "":
+                        if temp_w[0] == "'":
+                            temp_w = temp_w[1:]
+                        if temp_w in final_dict2:
+                            prev_count = final_dict2[temp_w]
+                            final_dict2.update({temp_w:prev_count+1})
                         else:
-                            break
+                            final_dict2.update({temp_w:1})
+                #
+                # print(tagged_tweet)
+                # print(token_split)
 
-                # print(temp_w)
-                temp_w = temp_w.lower()
+                nnp_list = [s[0] for s in tagged_tweet if s[1] == 'NNP' and s[0].lower() not in stop_dict]
 
-                if temp_w != "":
-                    if temp_w[0] == "'":
-                        temp_w = temp_w[1:]
-                    if temp_w in final_dict2:
-                        prev_count = final_dict2[temp_w]
-                        final_dict2.update({temp_w:prev_count+1})
+                for nnp_word in nnp_list:
+                    if nnp_word in final_dict:
+                        prev_count = final_dict[nnp_word]
+                        final_dict.update({nnp_word:prev_count+1})
                     else:
-                        final_dict2.update({temp_w:1})
-            #
-            # print(tagged_tweet)
-            # print(token_split)
+                        final_dict.update({nnp_word:1})
 
-            nnp_list = [s[0] for s in tagged_tweet if s[1] == 'NNP' and s[0].lower() not in stop_dict]
+        # print(award_data)
 
-            for nnp_word in nnp_list:
-                if nnp_word in final_dict:
-                    prev_count = final_dict[nnp_word]
-                    final_dict.update({nnp_word:prev_count+1})
-                else:
-                    final_dict.update({nnp_word:1})
+        if len(final_dict2) == 0:
+            sorted_list = sorted(final_dict.items(), key = itemgetter(1), reverse = True)
+            # print(sorted_list)
+            if len(sorted_list) >= 2:
+                # print("1")
+                result.update({host_word:sorted_list[0][0].lower()+" "+sorted_list[1][0].lower()})
+        else:
+            sorted_list = sorted(final_dict2.items(), key = itemgetter(1), reverse = True)
+            # print("2")
+            result.update({host_word:(sorted_list[0][0])})
 
-    # print(award_data)
+    return result    
 
-    if len(final_dict2) == 0:
-        sorted_list = sorted(final_dict.items(), key = itemgetter(1), reverse = True)
-        # print(sorted_list)
-        if len(sorted_list) >= 2:
-            # print("1")
-            result.update({host_word:sorted_list[0][0].lower()+" "+sorted_list[1][0].lower()})
-    else:
-        sorted_list = sorted(final_dict2.items(), key = itemgetter(1), reverse = True)
-        # print("2")
-        result.update({host_word:(sorted_list[0][0])})
-
-    return result
-# 
-# for award in OFFICIAL_AWARDS_1315:
-#     print(winners(data,award))
+print(winners(data,OFFICIAL_AWARDS_1315))

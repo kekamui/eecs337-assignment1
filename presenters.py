a254a2c89ca4bd04b87ca998ac3d51bbce669bd0
Jimin Yook <sprkal129@gmail.com>
2019-02-17 16:49:03 -0600

    presenters-

--- presenters.py
+++ presenters.py
@@ -300,6 +300,8 @@ def extract_presenters():
 		presenterfreqdist = FreqDist(awardWinnerPresenterDict["presenters"])
 		awardWinnerPresenterDict["presenters"] = [p for p,c in presenterfreqdist.most_common(2)]
 	pprint.pprint(awardWinnerPresenterList)
+
+	###return
 	return awardWinnerPresenterList
 extract_presenters()
 #print(text_data)
